39f4764c86e42b5c6ecc1918decea159541d092f
Ash White <ash@theadsmith.com>
2016-03-27 17:30:18 -0400

    recreated styling from previous iteration

---
+++ src/assets/js/contact-info.js
@@ -0,0 +1,5 @@
+var phoneText = "706" + "" + "-" + "" + "830" + "" + "-" + "" + "6424";
+$("#phone-number").html(phoneText);
+
+var emailText = "ashmwhite" + "" + "@" + "" + "gmail" + "" + "." + "" + "com";
+$("#email-address").attr("href", "mailto:" + emailText).html(emailText);
